06232a8a797f113b27e88ede26fa88ec7dc5a91f
Kuechenfenster <gismo.scherer@gmail.com>
2026-05-21 23:15:40 +0800

    fix: add Tailwind CSS as dev dependency, sync lock file, keep PostCss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+/**
+ * PostCSS configuration without external plugins.
+ * Used because the project relies on the Tailwind CSS CDN in index.html.
+ */
+module.exports = {
+  plugins: [],
 };
